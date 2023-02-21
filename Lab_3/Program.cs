e25c799ddfc8d7e16fc2bc52c38d5a8e7fd9eed1
Nikita60012 <96587581+Nikita60012@users.noreply.github.com>
2023-02-21 12:43:05 +0300

    Add files via upload

---
+++ Lab_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class Program
+    {
+        enum Day
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        enum Time
+        {
+            Morning,
+            Midday,
+            Evening,
+            Night
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите цифру от 1 до 7, чтобы выбрать день недели");
+            int dayOfTheWeek = Convert.ToInt32(Console.ReadLine()) - 1;
+            Console.WriteLine("Введите число от 0 до 23, чтобы выбрать который час");
+            int daytime = Convert.ToInt32(Console.ReadLine());
+            if(daytime >= 7 && daytime <= 12)
+            {
+                daytime = 0;
+            }
+            else if (daytime >= 13 && daytime <= 18)
+            {
+                daytime = 1;
+            }
+            else if (daytime >= 19 && daytime <= 23)
+            {
+                daytime = 2;
+            }
+            else if(daytime >= 0 && daytime <= 6)
+            {
+                daytime = 3;
+            }
+            Day day = (Day)(Enum.GetValues(typeof(Day)).GetValue(dayOfTheWeek));
+            Time time = (Time)(Enum.GetValues(typeof(Time)).GetValue(daytime));
+            Console.WriteLine($"Сейчас {day}, {time}");
+        }
+    }
+}
